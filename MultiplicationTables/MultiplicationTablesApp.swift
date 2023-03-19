4b19ad5a76a55412988ab06f870c5af4244b8e05
mberndt92 <maximilian_berndt@icloud.com>
2023-03-19 12:44:04 +0900

    Initial Commit

---
+++ MultiplicationTables/MultiplicationTablesApp.swift
@@ -0,0 +1,17 @@
+//
+//  MultiplicationTablesApp.swift
+//  MultiplicationTables
+//
+//  Created by Maximilian Berndt on 2023/03/19.
+//
+
+import SwiftUI
+
+@main
+struct MultiplicationTablesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
